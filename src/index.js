b889515b395342826eba79a1705b34b478ab24a1
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2020-09-17 14:20:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './App.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+//
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
